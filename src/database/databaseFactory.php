cae2014f1da8a4315ebcc132a196ac60bf9c36a4
Carlo Nicora <carlo.nicora@gmail.com>
2019-11-20 12:02:07 +0000

    database connection caching improvement

--- src/database/databaseFactory.php
+++ src/database/databaseFactory.php
@@ -32,23 +32,25 @@ class databaseFactory {
         $databaseName = $response->getDbToUse();
         $connection = self::$configurations->getDatabase($databaseName);
 
-        $dbConf = self::$configurations->getDatabaseConnectionString($databaseName);
+        if (!isset($connection)){
+            $dbConf = self::$configurations->getDatabaseConnectionString($databaseName);
 
-        if (!isset($connection) && isset($dbConf)){
-            $connection = new mysqli($dbConf['host'], $dbConf['username'], $dbConf['password'], $dbConf['dbName'], $dbConf['port']);
+            if (!empty($dbConf)){
+                $connection = new mysqli($dbConf['host'], $dbConf['username'], $dbConf['password'], $dbConf['dbName'], $dbConf['port']);
+            }
+
+            if (isset($connection) && !isset($connection->thread_id)) {
+                $connection->connect($dbConf['host'], $dbConf['username'], $dbConf['password'], $dbConf['dbName'], $dbConf['port']);
+            }
+
+            if (!isset($connection) || $connection->connect_errno) {
+                return null;
+            }
+
+            $connection->set_charset('utf8mb4');
+
+            self::$configurations->setDatabase($databaseName, $connection);
         }
-
-        if (isset($connection) && !isset($connection->thread_id)) {
-            $connection->connect($dbConf['host'], $dbConf['username'], $dbConf['password'], $dbConf['dbName'], $dbConf['port']);
-        }
-
-        if (!isset($connection) || $connection->connect_errno) {
-            return null;
-        }
-
-        $connection->set_charset('utf8mb4');
-
-        self::$configurations->setDatabase($databaseName, $connection);
 
         $response->setConnection($connection);
 
