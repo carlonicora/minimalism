f3996a214f2d0d0766845564867b48758795a26d
Carlo Nicora <carlo.nicora@gmail.com>
2020-01-17 12:11:05 +0000

    database table generator created

---
+++ src/databases/security/securityDb.php
@@ -0,0 +1,43 @@
+<?php
+namespace carlonicora\minimalism\databases\security;
+
+use carlonicora\minimalism\database\databaseFactory;
+use carlonicora\minimalism\databases\security\tables\auth;
+use carlonicora\minimalism\databases\security\tables\clients;
+use carlonicora\minimalism\exceptions\dbConnectionException;
+
+class securityDb {
+    /** @var auth */
+    private static $auth;
+
+    /** @var clients */
+    private static $clients;
+
+    /**
+     * @return auth
+     */
+    public static function auth(): auth {
+        if (!isset(self::$auth)){
+            try {
+                self::$auth = databaseFactory::create(auth::class);
+            } catch (dbConnectionException $e) {
+                self::$auth = null;
+            }
+        }
+        return self::$auth;
+    }
+
+    /**
+     * @return clients
+     */
+    public static function clients(): clients {
+        if (!isset(self::$clients)){
+            try {
+                self::$clients = databaseFactory::create(clients::class);
+            } catch (dbConnectionException $e) {
+                self::$clients = null;
+            }
+        }
+        return self::$clients;
+    }
+}
