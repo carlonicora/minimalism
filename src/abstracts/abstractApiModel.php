a392989f71915b6ca80c30a820d98e60f006c6a5
Carlo Nicora <carlo.nicora@gmail.com>
2020-02-13 10:58:47 +0000

    Methods defined as not abstract, returning 405 error if not implemented

--- src/abstracts/abstractApiModel.php
+++ src/abstracts/abstractApiModel.php
@@ -2,24 +2,38 @@
 
 namespace carlonicora\minimalism\abstracts;
 
+use carlonicora\minimalism\helpers\errorReporter;
+
 abstract class abstractApiModel extends abstractModel {
     /**
      * @return array
      */
-    abstract public function DELETE(): array;
+    public function DELETE(): array {
+        errorReporter::returnHttpCode(405, 'DELETE method not allowed');
+        exit;
+    }
 
     /**
      * @return array
      */
-    abstract public function GET(): array;
+    public function GET(): array {
+        errorReporter::returnHttpCode(405, 'GET method not allowed');
+        exit;
+    }
 
     /**
      * @return array
      */
-    abstract public function POST(): array;
+    public function POST(): array {
+        errorReporter::returnHttpCode(405, 'POST method not allowed');
+        exit;
+    }
 
     /**
      * @return array
      */
-    abstract public function PUT(): array;
+    public function PUT(): array {
+        errorReporter::returnHttpCode(405, 'PUT method not allowed');
+        exit;
+    }
 }
