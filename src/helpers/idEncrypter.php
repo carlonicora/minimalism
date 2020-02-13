ac176ab132b8d99802095f5fce45c19c1f77ade6
Carlo Nicora <carlo.nicora@gmail.com>
2020-02-13 21:12:57 +0000

    Encrypter initialised (based on hashids)

---
+++ src/helpers/idEncrypter.php
@@ -0,0 +1,34 @@
+<?php
+namespace carlonicora\minimalism\helpers;
+
+use carlonicora\minimalism\abstracts\abstractConfigurations;
+use Hashids\Hashids;
+
+class idEncrypter {
+    /** @var Hashids */
+    private $hashids;
+
+    /**
+     * idEncrypter constructor.
+     * @param abstractConfigurations $configurations
+     */
+    public function __construct(abstractConfigurations $configurations) {
+        $this->hashids = new Hashids($configurations->encrypterKey, $configurations->encrypterLength);
+    }
+
+    /**
+     * @param int $id
+     * @return string
+     */
+    public function encryptId(int $id): string {
+        return $this->hashids->encodeHex($id);
+    }
+
+    /**
+     * @param string $encryptedId
+     * @return int
+     */
+    public function decryptId(string $encryptedId): int {
+        return $this->hashids->decodeHex($encryptedId);
+    }
+}
