c5fd04ec338590b010c26f54dac90275939fb3de
Carlo Nicora <carlo.nicora@gmail.com>
2020-03-12 22:02:09 +0000

    links added

--- src/dataObjects/responseObject.php
+++ src/dataObjects/responseObject.php
@@ -8,8 +8,11 @@ class responseObject extends abstractResponseObject implements responseInterface
     /** @var array  */
     public array $data=[];
 
-    /** @var array|null  */
-    private ?array $meta=null;
+    /** @var array  */
+    private array $meta=[];
+
+    /** @var array  */
+    private array $links=[];
 
     /**
      * responseObject constructor.
@@ -17,10 +20,11 @@ class responseObject extends abstractResponseObject implements responseInterface
      * @param array|null $meta
      * @param string $httpStatusCode
      */
-    public function __construct(array $data = [], array $meta = null, string $httpStatusCode = self::HTTP_STATUS_200) {
+    public function __construct(array $data = [], array $meta=[], string $httpStatusCode = self::HTTP_STATUS_200) {
         $this->data = $data;
         $this->meta = $meta;
         $this->status = $httpStatusCode;
+        $this->links = [];
     }
 
     /**
@@ -35,11 +39,23 @@ class responseObject extends abstractResponseObject implements responseInterface
      * @param string $value
      */
     public function addMetaString(string $name, string $value): void {
-        if ($this->meta === null){
-            $this->meta = [];
-        }
-
         $this->meta[$name] = $value;
+    }
+
+    /**
+     * @param string $name
+     * @param string $url
+     * @param array|null $meta
+     */
+    public function addLink(string $name, string $url, array $meta=null): void {
+        if ($meta === null){
+            $this->links[$name] = $url;
+        } else {
+            $this->links[$name] = [
+                'href' => $url,
+                'meta' => $meta
+            ];
+        }
     }
 
     /**
@@ -50,7 +66,11 @@ class responseObject extends abstractResponseObject implements responseInterface
             'data' => $this->data
         ];
 
-        if ($this->meta !== null){
+        if (!empty($this->links)){
+            $response['links'] = $this->links;
+        }
+
+        if (!empty($this->meta)){
             $response['meta'] = $this->meta;
         }
 
