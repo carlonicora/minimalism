fc27ff82274ab91d69c234803cf1fb470032e4a1
skuzminich <8dJkzdHGpxxn>
2020-04-21 15:21:07 +0300

    PHP doc updated

--- src/core/services/exceptions/serviceNotFoundException.php
+++ src/core/services/exceptions/serviceNotFoundException.php
@@ -8,6 +8,8 @@ class serviceNotFoundException extends LogicException {
     /**
      * serviceNotFoundException constructor.
      * @param string $serviceName
+     * @param null $code
+     * @param Throwable|null $previous
      */
     public function __construct(string $serviceName, $code = null, Throwable $previous = null) {
         parent::__construct('Service ' . $serviceName . ' not found', $code, $previous);
