faa380a4b30251bb9548f3abd99bda7443614a4e
Carlo Nicora <carlo.nicora@gmail.com>
2020-05-04 16:29:44 +0100

    initial parameter type validation initialised

--- src/core/modules/abstracts/models/abstractModel.php
+++ src/core/modules/abstracts/models/abstractModel.php
@@ -4,6 +4,7 @@ namespace carlonicora\minimalism\core\modules\abstracts\models;
 use carlonicora\minimalism\core\services\exceptions\serviceNotFoundException;
 use carlonicora\minimalism\core\services\factories\servicesFactory;
 use carlonicora\minimalism\services\logger\logger;
+use DateTime;
 use Exception;
 use RuntimeException;
 
@@ -25,6 +26,13 @@ abstract class abstractModel {
 
     /** @var array  */
     protected array $passedParameters = [];
+
+    public const PARAMETER_TYPE_INT = 'validateIntParameter';
+    public const PARAMETER_TYPE_STRING = 'validateStringParameter';
+    public const PARAMETER_TYPE_BOOL = 'validateBoolParameter';
+    public const PARAMETER_TYPE_TIMESTAMP = 'validateTimestampParameter';
+    public const PARAMETER_TYPE_DATETIME = 'validateDatetimeParameter';
+    public const PARAMETER_TYPE_DECIMAL = 'validateDecimalParameter';
 
     /**
      * model constructor.
@@ -60,10 +68,13 @@ abstract class abstractModel {
                 $isParameterRequired = false;
                 $isParameterEncrypted = false;
 
+                $parameterValidation = $value['validation'] ?? self::PARAMETER_TYPE_STRING;
+
                 if (is_array($value)) {
                     $parameterName = $value['name'];
                     $isParameterRequired = $value['required'] ?? false;
                     $isParameterEncrypted = $value['encrypted'] ?? false;
+                    $parameterValidation = $value['validation'] ?? self::PARAMETER_TYPE_STRING;
                 }
 
                 $this->passedParameters[] = $parameterKey;
@@ -74,13 +85,80 @@ abstract class abstractModel {
                     ) {
                         $this->$parameterName = $this->decryptParameter($passedParameters[$parameterKey]);
                     } else {
-                        $this->$parameterName = $passedParameters[$parameterKey];
+                        $this->$parameterName = $this->$parameterValidation($passedParameters[$parameterKey]);
                     }
                 } elseif ($isParameterRequired){
                     throw new RuntimeException('Required parameter ' . $parameterName . ' missing.', 412);
                 }
             }
         }
+    }
+
+    /**
+     * @param $variable
+     * @return string
+     */
+    protected function validateStringParameter($variable) : string {
+        return (string)$variable;
+    }
+
+    /**
+     * @param $variable
+     * @return int
+     */
+    protected function validateIntParameter($variable) : int {
+        return (int)$variable;
+    }
+
+    /**
+     * @param $variable
+     * @return float
+     */
+    protected function validateDecimalParameter($variable) : float {
+        return (float)$variable;
+    }
+
+    /**
+     * @param $parameter
+     * @return bool
+     */
+    protected function validateBoolParameter($parameter) : bool {
+        return (bool)$parameter;
+    }
+
+    /**
+     * @param $parameter
+     * @return DateTime
+     * @throws Exception
+     */
+    protected function validateDatetimeParameter($parameter) : DateTime {
+        if ((is_int($parameter))){
+            return new DateTime('@' . $parameter);
+        }
+
+        if ((is_string($parameter))){
+            return new DateTime($parameter);
+        }
+
+        throw new RuntimeException('no valid parameter');
+    }
+
+    /**
+     * @param $parameter
+     * @return int
+     * @throws Exception
+     */
+    protected function validateTimestampParameter($parameter) : int {
+        if ((is_int($parameter))) {
+            return $parameter;
+        }
+
+        if ((is_string($parameter))) {
+            $date = new DateTime($parameter);
+            return $date->getTimestamp();
+        }
+
+        throw new RuntimeException('no valid parameter');
     }
 
     /**
