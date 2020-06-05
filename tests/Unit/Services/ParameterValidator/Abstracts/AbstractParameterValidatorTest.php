8121d71c26a5d249acc22487a947890e8f05f0e7
Marius Ghita <ghita.v.marius@gmail.com>
2020-06-05 20:17:51 +0300

    test AbstractParameterValidator renderParameter method

---
+++ tests/Unit/Services/ParameterValidator/Abstracts/AbstractParameterValidatorTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace CarloNicora\Minimalism\Tests\Unit\Services\ParameterValidator\Abstracts;
+
+use CarloNicora\Minimalism\Services\ParameterValidator\Abstracts\AbstractParameterValidator;
+use CarloNicora\Minimalism\Services\ParameterValidator\Objects\ParameterObject;
+use CarloNicora\Minimalism\Tests\Mocks\TestModel;
+use CarloNicora\Minimalism\Tests\Unit\AbstractTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class AbstractParameterValidatorTest extends AbstractTestCase
+{
+
+    protected const IDENTIFIER = 'test';
+
+    /**
+     * @param $parameterObjectParameter
+     * @return AbstractParameterValidator
+     */
+    public function getInstance($parameterObjectParameter = []): MockObject
+    {
+        return $this->getMockBuilder(AbstractParameterValidator::class)
+            ->setConstructorArgs([
+                $this->getServices(),
+                new ParameterObject(self::IDENTIFIER, $parameterObjectParameter)
+            ])
+            ->onlyMethods(['setParameter'])
+            ->getMockForAbstractClass();
+    }
+
+    public function getModelMock(): MockObject
+    {
+        return $this->getMockBuilder(TestModel::class)
+            ->setConstructorArgs([$this->getServices()])
+            ->onlyMethods(['addReceivedParameters'])
+            ->getMock();
+    }
+
+    public function testRenderParameterWithoutMatchingRequiredParameterObject()
+    {
+        $instance = $this->getInstance(['required' => 1]);
+        $modelMock = $this->getModelMock();
+
+        $this->expectException(\Exception::class);
+        $instance->renderParameter($modelMock, []);
+    }
+
+
+    public function testRenderParameterWithMatchingParameterObjectButNullValue()
+    {
+        $instance = $this->getInstance();
+        $modelMock = $this->getModelMock();
+
+        $instance->expects($this->never())->method('setParameter');
+        $modelMock->expects($this->once())->method('addReceivedParameters')->with(self::IDENTIFIER);
+
+        $instance->renderParameter($modelMock, [self::IDENTIFIER => null]);
+    }
+
+    public function testRenderParameterWithMatchingParameterObjectNotNull()
+    {
+        $value = 'test value';
+        $instance = $this->getInstance();
+        $modelMock = $this->getModelMock();
+
+        $instance->expects($this->once())->method('setParameter')->with($modelMock, $value);
+        $modelMock->expects($this->once())->method('addReceivedParameters')->with(self::IDENTIFIER);
+
+        $instance->renderParameter($modelMock, [self::IDENTIFIER => $value]);
+    }
+}
