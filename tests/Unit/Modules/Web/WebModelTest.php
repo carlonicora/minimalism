d54f3a5f9154568203bee8d1c3cbfcf8e76ee4a2
Marius Ghita <ghita.v.marius@gmail.com>
2020-06-08 18:52:46 +0300

    test WebModel class

---
+++ tests/Unit/Modules/Web/WebModelTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace CarloNicora\Minimalism\Tests\Unit\Modules\Web;
+
+use CarloNicora\JsonApi\Document;
+use CarloNicora\JsonApi\Objects\Error;
+use CarloNicora\Minimalism\Core\JsonApi\JsonApiResponse;
+use CarloNicora\Minimalism\Modules\Web\WebModel;
+use CarloNicora\Minimalism\Tests\Unit\AbstractTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+use Twig\Extension\ExtensionInterface;
+
+class WebModelTest extends AbstractTestCase
+{
+
+    private MockObject $instance;
+
+    public function setUp(): void
+    {
+        $this->instance = $this->getMockBuilder(WebModel::class)
+            ->setConstructorArgs([$this->getServices()])
+            ->getMockForAbstractClass();
+    }
+
+    public function testTwigExtensions()
+    {
+        $extension_one = $this->getMockBuilder(ExtensionInterface::class)->getMock();
+        $extension_two = $this->getMockBuilder(ExtensionInterface::class)->getMock();
+
+        $this->instance->addTwigExtension($extension_one);
+        $this->instance->addTwigExtension($extension_two);
+
+        $this->assertEquals([$extension_one, $extension_two], $this->instance->getTwigExtensions());
+    }
+
+    public function testGenerateData()
+    {
+        $this->assertInstanceOf(JsonApiResponse::class, $this->instance->generateData());
+    }
+
+    public function testPreRender()
+    {
+        $this->instance->preRender(); // should do nothing
+
+        $property = new \ReflectionProperty($this->instance, 'error');
+        $property->setAccessible(true);
+        /**
+         * @todo
+         * As an exception is rethrown based on the set Error object, at minimum a status code and detail need
+         * to be provided, otherwise the code will fail with a runtime error
+         */
+        $property->setValue($this->instance, new Error(null, 101, 'test detail'));
+
+        $this->expectException(\RuntimeException::class);
+        $this->instance->preRender();
+    }
+
+    public function testValidateJsonapiParameter()
+    {
+        $this->assertInstanceOf(Document::class, $this->instance->validateJsonapiParameter(null));
+    }
+}
