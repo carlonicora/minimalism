2a20698ea8cc720060749056f5c67341bccde2f5
Marius Ghita <ghita.v.marius@gmail.com>
2020-06-08 12:57:23 +0300

    test BoolValidator class

---
+++ tests/Unit/Services/ParameterValidator/Validators/BoolValidatorTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace CarloNicora\Minimalism\Tests\Unit\Services\ParameterValidator\Validators;
+
+use CarloNicora\Minimalism\Services\ParameterValidator\Objects\ParameterObject;
+use CarloNicora\Minimalism\Services\ParameterValidator\Validators\BoolValidator;
+use CarloNicora\Minimalism\Tests\Mocks\TestModel;
+use CarloNicora\Minimalism\Tests\Unit\AbstractTestCase;
+
+
+class BoolValidatorTest extends AbstractTestCase
+{
+
+    /**
+     * @dataProvider provider
+     * @param $output
+     * @param $input
+     */
+    public function testSetParameter($output, $input)
+    {
+        $parameterName = 'test';
+
+        $instance = new BoolValidator($this->getServices(), new ParameterObject($parameterName, []));
+
+        $mock = $this->getMockBuilder(TestModel::class)
+            ->setConstructorArgs([$this->getServices()])
+            ->getMock();
+        $mock->expects($this->once())->method('setParameter')->with($parameterName, $output);
+
+        $instance->setParameter($mock, $input);
+    }
+
+
+    public function provider()
+    {
+        return [
+            [ false, "" ],
+            [ false, null ],
+            [ false, [] ],
+            [ false, 0 ],
+            [ false, "0" ],
+            [ true,  ['a','b'] ],
+            [ true,  1 ],
+            [ true,  -1 ],
+            [ true,  "1" ],
+            [ true,  "-1" ]
+        ];
+    }
+}
