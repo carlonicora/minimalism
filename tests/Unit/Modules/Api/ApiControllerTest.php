19b7dc47e2ce21069b79048f83fa85195053bdf6
Marius Ghita <ghita.v.marius@gmail.com>
2020-06-08 18:35:31 +0300

    test partially ApiController class

---
+++ tests/Unit/Modules/Api/ApiControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CarloNicora\Minimalism\Tests\Unit\Modules\Api;
+
+use CarloNicora\Minimalism\Modules\Api\ApiController;
+use CarloNicora\Minimalism\Tests\Unit\AbstractTestCase;
+
+class ApiControllerTest extends AbstractTestCase
+{
+
+    public function testInitialiseModelWithDefaults()
+    {
+        $instance = new ApiController($this->getServices());
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Model not found: index');
+        $instance->initialiseModel();
+    }
+}
