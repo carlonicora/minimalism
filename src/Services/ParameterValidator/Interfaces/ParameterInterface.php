dac5c7a61d5c86b36a8306d0e37615f74f5a30ce
Carlo Nicora <carlo.nicora@gmail.com>
2020-06-17 14:28:30 +0100

    ParameterInterface added

---
+++ src/Services/ParameterValidator/Interfaces/ParameterInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace CarloNicora\Minimalism\Services\ParameterValidator\Interfaces;
+
+interface ParameterInterface
+{
+    public const NAME = 'name';
+    public const IS_REQUIRED = 'required';
+    public const IS_ENCRYPTED = 'encrypted';
+    public const TYPE = 'type';
+    public const VALIDATOR = 'validator';
+}
