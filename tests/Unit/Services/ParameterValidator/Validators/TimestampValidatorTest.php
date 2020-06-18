015b619d51c29d587f9468517d0df5a37028bd61
Marius Ghita <ghita.v.marius@gmail.com>
2020-06-18 16:39:35 +0300

    simplify TimestampValidator tests

--- tests/Unit/Services/ParameterValidator/Validators/TimestampValidatorTest.php
+++ tests/Unit/Services/ParameterValidator/Validators/TimestampValidatorTest.php
@@ -2,44 +2,29 @@
 
 namespace CarloNicora\Minimalism\Tests\Unit\Services\ParameterValidator\Validators;
 
-use CarloNicora\Minimalism\Services\ParameterValidator\Objects\ParameterObject;
 use CarloNicora\Minimalism\Services\ParameterValidator\Validators\TimestampValidator;
-use CarloNicora\Minimalism\Tests\Mocks\TestModel;
 use CarloNicora\Minimalism\Tests\Unit\AbstractTestCase;
-use Exception;
 use function date;
 
 class TimestampValidatorTest extends AbstractTestCase
 {
 
-    /**
-     * @note very much similar to the DateTimeValidatorTest, as such the method is duplicated
-     * @throws Exception
-     */
-    public function testSetParameter()
+    public function testTransformValue()
     {
-        $parameterName = 'test';
         $dateValue = date('Y-m-d H:i:s');
         $dateValueHoursMinutesSeconds = date('H:i:s');
 
         $instance = new TimestampValidator($this->getServices());
 
-        $mock = $this->getMockBuilder(TestModel::class)
-            ->setConstructorArgs([$this->getServices()])
-            ->getMock();
+        $this->assertNull($instance->transformValue(null));
+        $this->assertEquals(\strtotime($dateValue), $instance->transformValue($dateValue));
 
-        $mock->expects($this->at(0))->method('setParameter')->with(
-            $this->identicalTo($parameterName),
-            $this->callback(function($timestamp) use($dateValue) {
-                return date('Y-m-d H:i:s', $timestamp) === $dateValue;
-            })
+        $this->expectException(\Exception::class);
+        $instance->transformValue($dateValueHoursMinutesSeconds);
+
+        $this->assertEquals(
+            \strtotime($dateValueHoursMinutesSeconds),
+            $instance->transformValue(\strtotime($dateValueHoursMinutesSeconds))
         );
-
-
-        $instance->setParameter(new ParameterObject($parameterName, []), $mock, $dateValue);
-
-        $this->expectException(Exception::class);
-        $this->expectExceptionMessage("Parameter Type mismatch: $parameterName");
-        $instance->setParameter(new ParameterObject($parameterName, []), $mock, $dateValueHoursMinutesSeconds);
     }
 }
