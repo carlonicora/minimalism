ef8d4bcb8b3ca498ed74834d0b0dc8425fd14c07
Carlo Nicora <carlo.nicora@gmail.com>
2020-12-10 15:26:08 +0100

    initial version 11.3

---
+++ src/Services/Data/Abstracts/AbstractPool.php
@@ -0,0 +1,36 @@
+<?php
+namespace CarloNicora\Minimalism\Services\Data\Abstracts;
+
+use CarloNicora\Minimalism\Core\Services\Factories\ServicesFactory;
+use Exception;
+
+abstract class AbstractPool
+{
+    /** @var ServicesFactory  */
+    protected ServicesFactory $services;
+
+    /** @var array  */
+    protected array $pool=[];
+
+    /**
+     * UsersLoader constructor.
+     * @param ServicesFactory $services
+     */
+    public function __construct(ServicesFactory $services)
+    {
+        $this->services = $services;
+    }
+
+    /**
+     * @param string $className
+     * @return mixed
+     * @throws Exception
+     */
+    public function loadPoolElement(string $className)
+    {
+        if (!array_key_exists($className, $this->pool)) {
+            $this->pool[$className] = new $className($this->services);
+        }
+        return $this->pool[$className];
+    }
+}
