8b24fcb4255f7996c8cb29900af3adb6e24e0719
Carlo Nicora <carlo.nicora@gmail.com>
2021-01-02 11:57:09 +0100

    Data Interface extended

--- src/Interfaces/DataInterface.php
+++ src/Interfaces/DataInterface.php
@@ -8,4 +8,58 @@ interface DataInterface
      * @return TableInterface
      */
     public function create(string $dbReader): TableInterface;
+
+    /**
+     * @param string $tableInterfaceClassName
+     * @param string $functionName
+     * @param array $parameters
+     * @param CacheBuilderInterface|null $cacheBuilder
+     * @return array
+     */
+    public function read(
+        string $tableInterfaceClassName,
+        string $functionName,
+        array $parameters,
+        ?CacheBuilderInterface $cacheBuilder=null,
+    ): array;
+
+    /**
+     * @param string $tableInterfaceClassName
+     * @param string $functionName
+     * @param array $parameters
+     * @param CacheBuilderInterface|null $cacheBuilder
+     */
+    public function update(
+        string $tableInterfaceClassName,
+        string $functionName,
+        array $parameters,
+        ?CacheBuilderInterface $cacheBuilder=null,
+    ): void;
+
+    /**
+     * @param string $tableInterfaceClassName
+     * @param string $functionName
+     * @param array $parameters
+     * @param CacheBuilderInterface|null $cacheBuilder
+     */
+    public function delete(
+        string $tableInterfaceClassName,
+        string $functionName,
+        array $parameters,
+        ?CacheBuilderInterface $cacheBuilder=null,
+    ): void;
+
+    /**
+     * @param string $tableInterfaceClassName
+     * @param string $functionName
+     * @param array $parameters
+     * @param CacheBuilderInterface|null $cacheBuilder
+     * @return array
+     */
+    public function insert(
+        string $tableInterfaceClassName,
+        string $functionName,
+        array $parameters,
+        ?CacheBuilderInterface $cacheBuilder=null,
+    ): array;
 }
