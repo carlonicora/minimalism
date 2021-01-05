11d557b0673a4bd620584f8aa2275d7761bce393
Carlo Nicora <carlo.nicora@gmail.com>
2021-01-05 11:49:19 +0100

    RecordNotFoundException added

---
+++ src/Exceptions/RecordNotFoundException.php
@@ -0,0 +1,19 @@
+<?php
+namespace CarloNicora\Minimalism\Exceptions;
+
+use Exception;
+use Throwable;
+
+class RecordNotFoundException extends Exception
+{
+    /**
+     * DbRecordNotFoundException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code!==0?$code:404, $previous);
+    }
+}
