eb7cdb850a40a102256bc7ecabfed89b58f624b8
Sergey Kuzminich <SergeyKuzminich@yandex.ru>
2021-11-04 15:10:57 +0300

    getApplicationUrl and getApiUrl methods in DefaultServiceInterface

--- src/Interfaces/DefaultServiceInterface.php
+++ src/Interfaces/DefaultServiceInterface.php
@@ -7,4 +7,14 @@ interface DefaultServiceInterface
      * @return array
      */
     public function getDelayedServices(): array;
+
+    /**
+     * @return string|null
+     */
+    public function getApplicationUrl(): ?string;
+
+    /**
+     * @return string|null
+     */
+    public function getApiUrl(): ?string;
 }
