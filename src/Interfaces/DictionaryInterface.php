bd2dbebf8289f00282c64c5c34133bf588a3bf16
Carlo Nicora <carlo.nicora@gmail.com>
2023-02-09 16:05:24 +0100

    feat: add DictionaryInterface

---
+++ src/Interfaces/DictionaryInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace CarloNicora\Minimalism\Interfaces;
+
+interface DictionaryInterface
+{
+    /**
+     * @return string
+     */
+    public function getEndpoint(
+    ): string;
+}
